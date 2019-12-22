1377c7d411ada03b4ed306d0a94a0615b3c3b740
danailstratirv <stratyd@abv.bg>
2019-12-22 22:54:00 +0200

    Exercise Answer

---
+++ Interview Tasks and Preparations/Exercise_Answer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExerciseAnswer
+{
+    public class Answer
+    {
+        /// Executes the service with the given​​​​​​‌​​​‌​‌​​​‌‌‌​‌‌‌‌‌‌​​​‌‌ connection.
+        public void Run(Service s, Connection c)
+        {
+            s.SetConnection(c);
+            try
+            {
+                s.Execute();
+
+            }
+            catch (Exception)
+            {
+
+                c.Rollback();
+            }
+            finally
+            {
+                c.Commit();
+            }
+        }
+    }
+
+    /// Definition of a service
+    public interface Service
+    {
+        void Execute();
+        void SetConnection(Connection c);
+    }
+
+    /// Definition of a connection
+    public interface Connection
+    {
+        void Commit();
+        void Rollback();
+        void Close();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
