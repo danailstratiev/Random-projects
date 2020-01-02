5538b47ff4f38b50bb9eb60619adad0403970708
danailstratirv <stratyd@abv.bg>
2020-01-02 20:09:24 +0200

    30 Days of Code day 1 Data Types

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_1_Data_Types/Day_1_Data_Types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day_1_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+
+            // Declare second integer, double, and String variables.
+
+            // Read and save an integer, double, and String to your variables.
+
+            // Print the sum of both integer variables on a new line.
+
+            // Print the sum of the double variables on a new line.
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+
+            int j = int.Parse(Console.ReadLine());
+            double f = double.Parse(Console.ReadLine());
+            string t = Console.ReadLine();
+
+            Console.WriteLine(i+j);
+            Console.WriteLine($"{d+f:F1}");
+            Console.WriteLine(s+t);
+        }
+    }
+}
