0e8aa979fcdc805bd363e920ae9a5072636016ab
danailstratirv <stratyd@abv.bg>
2020-01-07 09:17:02 +0200

    Day 6 Let's Review

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_6_Lets_Review/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day_6_Lets_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var word = Console.ReadLine();
+                var miniWordEven = "";
+                var miniWordOdd = "";
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        miniWordEven += word[j];
+                    }
+                    else
+                    {
+                        miniWordOdd += word[j];
+                    }
+                }
+
+                Console.WriteLine($"{miniWordEven} {miniWordOdd}");
+            }
+        }
+    }
+}
