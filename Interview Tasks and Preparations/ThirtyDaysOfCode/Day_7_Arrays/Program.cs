02a5e37b9ff5086c6e32b515ded8dcc1bff038a5
danailstratirv <stratyd@abv.bg>
2020-01-08 13:51:13 +0200

    Day 7 Arrays

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_7_Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+
+            Console.WriteLine(string.Join(" ", arr.Reverse().ToArray()));
+        }
+    }
+}
