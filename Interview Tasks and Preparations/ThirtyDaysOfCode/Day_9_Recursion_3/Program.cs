27e078df628ef838276e1c36bac40b36c595117b
danailstratirv <stratyd@abv.bg>
2020-01-11 06:49:33 +0200

    Day_9_Recursion_3

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_9_Recursion_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Day_9_Recursion_3
+{
+    class Program
+    {
+        // Complete the factorial function below.
+        static int factorial(int n)
+        {
+            var m = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                m *= i;
+            }
+
+            return m;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
