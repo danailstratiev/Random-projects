3cdb4b55b98ebeca5a8526dca6a30c23dc21fd44
danailstratirv <stratyd@abv.bg>
2020-01-11 10:26:03 +0200

    Day_10_Binary_Numbers

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_10_Binary_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_10_Binary_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var binary = Convert.ToString(n, 2);
+            var countOfOne = 0;
+            var maxSequence = 1;
+
+            foreach (var digit in binary)
+            {
+                if (digit == '1')
+                {
+                    countOfOne++;
+                    if (countOfOne > maxSequence)
+                    {
+                        maxSequence = countOfOne;
+                    }
+                }
+                else
+                {
+                    countOfOne = 0;
+                }
+            }
+            Console.WriteLine(maxSequence);
+        }
+    }
+}
