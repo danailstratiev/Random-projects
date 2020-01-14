d2d1369c0eb3b8d6c2ebb542c55a90a16883dbe4
danailstratirv <stratyd@abv.bg>
2020-01-14 17:35:38 +0200

    Day_13_Abstract_Classes

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_13_Abstract_Classes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Day_13_Abstract_Classes
+{
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+
+    }
+
+    class MyBook : Book
+    {
+        protected int price;
+
+        public MyBook(String t, String a, int price)
+            : base(t, a)
+        {
+            this.title = t;
+            this.author = a;
+            this.price = price;
+        }
+        public override void display()
+        {
+            Console.WriteLine($"Title: {this.title}");
+            Console.WriteLine($"Author: {this.author}");
+            Console.WriteLine($"Price: {this.price}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
