7ebb6840464ad0819d1f82fe99c303aa63651b3c
danailstratirv <stratyd@abv.bg>
2020-01-15 21:46:07 +0200

    Day_14_Scope

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_14_Scope/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_14_Scope
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void computeDifference()
+        {
+            maximumDifference = elements.Max() - elements.Min();
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
