75758684aba207cf5bf947a184903afb007d4322
danailstratirv <stratyd@abv.bg>
2020-01-16 23:53:39 +0200

    LinkedList methods added

--- Interview Tasks and Preparations/ThirtyDaysOfCode/Day_15_Linked_List/LinkedList.cs
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_15_Linked_List/LinkedList.cs
@@ -9,11 +9,11 @@ namespace Day_15_Linked_List
         Node head;
         int count;
 
-        public LinkedList()
-        {
-            head = null;
-            count = 0;
-        }
+        //public LinkedList()
+        //{
+        //    head = null;
+        //    count = 0;
+        //}
 
         public LinkedList(Node newHead)
         {
@@ -31,6 +31,46 @@ namespace Day_15_Linked_List
                 current = current.GetNext();
             }
             current.SetNext(temp);
+            count++;
+        }
+
+        public int Get(int index)
+        {
+            if (index <= 0)
+            {
+                return -1;
+            }
+
+            Node current = head;
+
+            for (int i = 1; i < index; i++)
+            {
+                current = current.GetNext();
+            }
+
+            return current.GetData();
+        }
+
+        public int Size()
+        {
+            return count;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.head == null;
+        }
+
+        public void Remove()
+        {
+            Node current = head;
+
+            while (current.GetNext().GetNext() != null)
+            {
+                current = current.GetNext();
+            }
+            current.SetNext(null);
+            count--;
         }
     }
 
