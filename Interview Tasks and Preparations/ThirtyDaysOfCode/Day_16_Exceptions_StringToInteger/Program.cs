8a5d41eb9285be59b243f5574365efa870291dc7
danailstratirv <stratyd@abv.bg>
2020-01-17 21:05:03 +0200

    Day_16_Exceptions_StringToInteger

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_16_Exceptions_StringToInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_16_Exceptions_StringToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");                
+            }
+        }
+    }
+}
