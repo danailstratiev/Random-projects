358212ea59d291b81d9dff1d952445b17c6dba1f
danailstratirv <stratyd@abv.bg>
2020-01-22 12:12:01 +0200

    Day_21_Generics

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_21_Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day_21_Generics
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+        }
+
+        public static void PrintArray <T> (T[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
