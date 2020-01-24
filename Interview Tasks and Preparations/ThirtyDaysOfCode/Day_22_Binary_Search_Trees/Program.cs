7c3b4141eb58f44beafaef825f860e4afb7aa688
danailstratirv <stratyd@abv.bg>
2020-01-24 03:20:18 +0200

    Final solution bts

--- Interview Tasks and Preparations/ThirtyDaysOfCode/Day_22_Binary_Search_Trees/Program.cs
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_22_Binary_Search_Trees/Program.cs
@@ -2,11 +2,68 @@
 
 namespace Day_22_Binary_Search_Trees
 {
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
     class Program
     {
+        static int getHeight(Node root)
+        {
+            //Write your code here
+
+            if (root == null)
+            {
+                return -1;
+            }
+
+            var leftHeight = getHeight(root.left);
+            var rightHeight = getHeight(root.right);
+
+            var height = Math.Max(leftHeight, rightHeight) + 1;
+
+            return height;
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
         }
     }
 }
