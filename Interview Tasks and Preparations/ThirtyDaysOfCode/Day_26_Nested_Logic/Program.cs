b39d06987d4340a305646b1535a741b7826592cf
danailstratirv <stratyd@abv.bg>
2020-01-27 10:14:29 +0200

    Day_26_Nested_Logic

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_26_Nested_Logic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Day_26_Nested_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var realDates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var expectedDates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var realDay = realDates[0];
+            var realMonth = realDates[1];
+            var realYear = realDates[2];
+
+            var expectedDay = expectedDates[0];
+            var expectedMonth = expectedDates[1];
+            var expectedYear = expectedDates[2];
+
+            var fine = 0;
+
+            if (realYear < expectedYear || realMonth <= expectedMonth && realYear <= expectedYear && realDay <= expectedDay)
+            {
+                fine = 0;
+            }
+            else if (realMonth == expectedMonth && realYear == expectedYear && realDay > expectedDay)
+            {
+                fine = 15 * (realDay - expectedDay);
+            }
+            else if (realMonth > expectedMonth && realYear == expectedYear)
+            {
+                fine = 500 * (realMonth - expectedMonth);
+            }
+            else
+            {
+                fine = 10000;
+            }
+
+            Console.WriteLine(fine);
+        }
+    }
+}
