64618146b720c6e83a33cb07bfc32798f8d4c689
danailstratirv <stratyd@abv.bg>
2020-01-29 09:19:57 +0200

    Day_28_RegEx_Patterns_and_Intro_to_Databases

---
+++ Interview Tasks and Preparations/ThirtyDaysOfCode/Day_28_RegEx_Patterns_and_Intro_to_Databases/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Day_28_RegEx_Patterns_and_Intro_to_Databases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            var names = new List<string>();
+
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+                string firstName = firstNameEmailID[0];
+
+                string emailID = firstNameEmailID[1];
+
+                var pattern = @"[a-z]+@gmail.com";
+
+                if (Regex.IsMatch(emailID, pattern))
+                {
+                    names.Add(firstName);
+                }
+            }
+        }
+    }
+}
