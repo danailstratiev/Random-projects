2465ffbd32caf6305b2458c86f88eed11a3349a4
danailstratirv <stratyd@abv.bg>
2020-02-21 01:33:17 +0200

    Zoo task

---
+++ Interview Tasks and Preparations/ConsoleApp1/Mammals/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Mammals
+{
+    public abstract class Mammal
+    {
+        private void Eat()
+        {
+            Console.WriteLine("The Mammal is eating");
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+        public int conversion(int n)
+        {
+            int divisor = -1;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (!IsPrimeNumber(i) && n % i == 0)
+                {
+
+                    divisor = i;
+
+                }
+            }
+
+            return divisor;
+        }
+        public bool IsPrimeNumber(int number)
+        {
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    public class Whale : Mammal
+    {
+        protected void Eat()
+        {
+            Console.WriteLine("The whale is eating");
+        }
+    }
+}
---
+++ Interview Tasks and Preparations/Zoo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Zoo
+{
+    class Result
+    {
+        public int output1;
+        public int output2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var 
+
+            Console.WriteLine(45/60);
+            Console.WriteLine(45%60);
+        }
+
+        public Result calculateTime(int input1, int[] input2,int[] input3 ,int[] input4,int[] input5)
+        {
+            var timeValues = new int[3];
+            var finalTime = new Result()
+            {
+                output1 = 0,
+                output2 = 0
+            };
+            for (int i = 1; i <= input1; i++)
+            {
+                if (input1 == 2)
+                {
+                    timeValues = input2;
+                    
+                }
+                else if (input1 == 3)
+                {
+                    timeValues = input3;
+                }
+                else if (input1 == 4)
+                {
+                    timeValues = input4;
+                }
+                else
+                {
+                    timeValues = input5;
+                }
+                var temp = getTime(timeValues);
+                if (temp.output1 > finalTime.output1)
+                {
+                    finalTime = temp;
+                }
+            }
+            return finalTime;
+
+            
+
+        }
+        public Result getTime(int[] timeValues)
+        {
+            var hour = timeValues[0];
+            var minutes = timeValues[1];
+            var duration = timeValues[2];
+
+            var extraHours = duration / 60;
+            var extraMinutes = duration % 60;
+
+            hour += extraHours;
+            minutes += extraMinutes;
+
+            if (minutes > 60)
+            {
+                minutes -= 60;
+                hour++;
+            }
+
+            if (hour > 23)
+            {
+                hour -= 24;
+            }
+
+            var result = new Result()
+            {
+                output1 = hour,
+                output2 = minutes
+            };
+            return result;
+
+        }
+    }
+}
