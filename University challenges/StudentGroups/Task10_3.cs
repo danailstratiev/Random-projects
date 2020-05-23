e7725c2587358c22ade4c470a0a5ec89ff2d304a
danailstratirv <stratyd@abv.bg>
2020-05-23 23:35:14 +0300

    Student Groups.

---
+++ University challenges/StudentGroups/Task10_3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StudentGroups
+{
+    class Task10_3
+    {
+        static void Main(string[] args)
+        {
+            int n_groups = 5;
+            string[] count_students =  new string[n_groups];
+
+            for (int i = 0; i < n_groups; i++)
+            {
+                count_students[i] = Console.ReadLine();
+            }
+
+            int[][] marks = new int[n_groups][];
+        }
+    }
+}
