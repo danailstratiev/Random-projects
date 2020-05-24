eb3046652f04eb7c66dc65e43434425a0a4a8f91
danailstratirv <stratyd@abv.bg>
2020-05-24 23:46:42 +0300

    Homework 11/ Task 1

---
+++ University challenges/Homework_11/Task1/Task11_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] workers = new Worker[4];
+            double salarySum = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Worker worker = new Worker();
+                worker.Name = Console.ReadLine();
+                worker.WorkPosition = Console.ReadLine();
+                worker.Salary = double.Parse(Console.ReadLine());
+                salarySum += worker.Salary;
+            }
+
+            Console.WriteLine(salarySum/4);
+        }
+    }
+
+    public class Worker
+    {
+        public string Name { get; set; }
+
+        public string WorkPosition { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
