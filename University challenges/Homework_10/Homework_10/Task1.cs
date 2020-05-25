ca5fa512a8bba8b22edf8b65e940d25d81d22e5b
danailstratirv <stratyd@abv.bg>
2020-05-26 00:25:23 +0300

    Homework 10 complete.

---
+++ University challenges/Homework_10/Homework_10/Task1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Homework_10
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int rows = 5;
+            int columns = 5;
+            var matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    matrix[i, j] = random.Next(10, 100);
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            int sum = int.MinValue;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int rowSum = 0;
+
+                for (int j = 0; j <= i; j++)
+                {
+                    rowSum += matrix[i, j];
+
+                }
+                Console.WriteLine(rowSum);
+
+                if (rowSum > sum)
+                {
+                    sum = rowSum;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
